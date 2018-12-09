1aa0968388694c5bf2e7eae04ed1ca0d473f6cef
Branson Fox <32143148+bransonf@users.noreply.github.com>
2018-12-08 23:58:38 -0600

    newprj

---
+++ R/newprj.R
@@ -0,0 +1,62 @@
+### Define some naming variables
+
+user <- Sys.getenv("LOGNAME")
+projName <- basename(getwd())
+
+### Create directories
+
+dir.create("data")
+dir.create("docs")
+dir.create("results")
+dir.create("source")
+
+### Creating a Notebook by Default (Add proj name paste statement)
+
+file.create(paste0("docs/",projName ,".Rmd"))
+fileConn <- file(paste0("docs/",projName ,".Rmd"))
+writeLines(c(
+  "---",
+  paste0("title: ",'"', projName,'"'),
+  paste0("author: ", '"', user, '"'),
+  paste0("date: '(`r format","(Sys.time(),",' "%B %d, %Y")',"`)'"),
+  "output: ",
+  "  github_document: default",
+  "  html_notebook: default",
+  "---",
+  "",
+  "## Introduction",
+  paste0("This is the R Notebook for ", projName,"."),
+  "",
+  "## Dependencies",
+  "The following code chunk loads the packages which this project depends on.",
+  "",
+  "```{r dependencies, message=FALSE}",
+  "library()",
+  "```",
+  "",
+  "## Load Data",
+  "The following code chunk loads into the global environment the data used for this project.",
+  "",
+  "```{r load data}",
+  "",
+  "```"
+), fileConn)
+close(fileConn)
+
+### For the README.md default
+
+file.create("README.md")
+fileConn <- file("README.md")
+writeLines(c(
+  paste0("This folder contains the following directories for ","`", projName, "`"),
+  "",
+  "* `data/` contains the data used in this project.",
+  "* `docs/` contains the Rnotebook used for this project.",
+  "* `results/` contains the output results of this project.",
+  "* `source/` contains the script source code for this project."
+), fileConn)
+close(fileConn)
+
+### and to clear environment
+
+rm(fileConn, user, projName)
